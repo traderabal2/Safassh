421c2970c3259ee20beac43e22ef3febb85f8fe5
traderabal2 <safatrader88@gmail.com>
2024-08-23 16:30:45 +0700

    Update m-ip.sh

--- menu/m-ip.sh
+++ menu/m-ip.sh
@@ -9,7 +9,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/traderabal2/Permission/main/seler"
+data_ip="https://raw.githubusercontent.com/traderabal2/Permission/main/1p"
 checking_sc() {
   useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
@@ -38,7 +38,7 @@ Repo1="https://raw.githubusercontent.com/traderabal2/Permission/main/"
 export MYIP=$( curl -s https://ipinfo.io/ip/ )
 SELLER=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $2}')
 Exp100=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $3}')
-data_ip="https://raw.githubusercontent.com/traderabal2/Permission/main/seler"
+data_ip="https://raw.githubusercontent.com/traderabal2/Permission/main/ip"
 d2=$(date -d "$date_list" +"+%s")
 d1=$(date -d "$Exp" +"+%s")
 dayleft=$(( ($d1 - $d2) / 86400 ))
